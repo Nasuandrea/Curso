58ae697448a17ddd84e02872d574a64e5f0eb1eb
Nasua <acollazocacho@gmail.com>
2025-11-13 10:07:05 +0100

    gestor biblioteca, sol

--- Modulo-02/01-Gestor-Biblioteca/gestor-biblioteca.js
+++ Modulo-02/01-Gestor-Biblioteca/gestor-biblioteca.js
@@ -2,18 +2,26 @@ class GestorBiblioteca {}
 
 const libros = [
   {
-    titulo: "",
-    autor: "",
+    titulo: "El señor de los anillos",
+    autor: "Tolkien",
     prestado: false,
     fechaInicioPrestamo: "",
     fechaFinPrestamo: "",
   },
+    {
+    titulo: "Las dos torres",
+    autor: "Tolkien",
+    prestado: false,
+    fechaInicioPrestamo: "",
+    fechaFinPrestamo: "",
+  }
 ];
 let titulo;
 let autor;
 let prestado = false;
 let fechaInicioPrestamo;
 let fechaFinPrestamo;
+let div = document.getElementById("info");
 
 function registrarLibro() {
   titulo = prompt("Título del libro: ");
@@ -23,20 +31,25 @@ function registrarLibro() {
       alert("Ese libro ya esta registrado");
       return registrarLibro();
     } else {
-      libros.push((l.titulo = titulo), (l.autor = autor));
+      libros.push((l.titulo = titulo), (l.autor = autor), l.prestado=false, l.fechaFinPrestamo="", l.fechaInicioPrestamo="");
     }
   });
 }
 
 function buscarPorTitulo() {
   titulo = prompt("Título a buscar: ");
-  libros.forEach((l) => {
     let tituloBuscado = titulo.toLowerCase();
     let filtro = libros.filter((libro) =>
-      libros.titulo.toLowerCase().includes(tituloBuscado)
+      libro.titulo.toLowerCase().includes(tituloBuscado)
     );
-    if (filtro){
-        prompt("El libro buscado es: "+ l.autor + ", "+ l.titulo)
+    if (tituloBuscado){
+      filtro.forEach((f)=> div.innerHTML =`
+        El libro buscado es:<br> Autor: ${f.autor}<br> Título: ${f.titulo};`);
+    }else{
+      div.innerHTML="EL libro buscado no existe";
     }
-  });
+}
+
+function prestarLibro(){
+  
 }
