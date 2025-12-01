6b3d69bf294809b7f2c68d80d99044771f1f9cf8
Nasua <acollazocacho@gmail.com>
2025-12-01 11:12:38 +0100

    fix else

--- Modulo-02/03-Comandos Linux y BBDD -  Sistema escolar/sistema-estudiantes/api/crear-estudiante.php
+++ Modulo-02/03-Comandos Linux y BBDD -  Sistema escolar/sistema-estudiantes/api/crear-estudiante.php
@@ -1,16 +1,17 @@
-<?php 
+<?php
+
 /**
  * api/crear-estudiante.php
  * propósito: Api para crear estudiantes en la base de datos
  * 
  * retorna: info (JSON) a javaScript para que procese la respuesta
-*/
+ */
 
 //Incluir conexion 
 include '../conexion.php';
 
 //Verificar que la petición sea el método POST
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     //Obtener y limpiar los datos del formulario
     $dni = trim($_POST['dni']);
     $nombre = trim($_POST['nombre']);
@@ -18,37 +19,36 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $edad = intval($_POST['edad']);
     $curso = trim($_POST['curso']);
     //Validar que todos los campos requeridos esten presentes
-    if (empty($dni) || empty($nombre) || empty($apellido) || empty($edad) || empty($curso)){
+    if (empty($dni) || empty($nombre) || empty($apellido) || empty($edad) || empty($curso)) {
         echo json_encode([
             'exito' => 'false',
-            'mensaje'=> 'todos los campos son obligatorios'
+            'mensaje' => 'todos los campos son obligatorios'
         ]);
         exit;
     }
     //Verificar si el dni existe en el sistema
     $consulta = "INSERT INTO estudiantes(dni,nombre,apellido,edad,curso)VALUES(?,?,?,?,?)";
     //Verificar conexion a la bbdd
-    $stmt = $conexion -> prepare($consulta);
-    $stmt = bind_param("sssis",$dni,$nombre,$apellido,$edad,$curso);
+    $stmt = $conexion->prepare($consulta);
+    $stmt = bind_param("sssis", $dni, $nombre, $apellido, $edad, $curso);
 
     //Ejecutar consulta
-    if ($stmt -> execute()) {
+    if ($stmt->execute()) {
         echo json_encode([
             'exito' => 'true',
-            'mensaje'=> 'Estudiante creado exitosamente',
-            'id' => $stmt -> insert_id //ID del nuevo estudiante
-            ]);
-    }else{
-        echo json_encode([
-            'exito'=> 'false',
-            'mensaje' => 'Error al crear estudiante:'.$conexion -> error
+            'mensaje' => 'Estudiante creado exitosamente',
+            'id' => $stmt->insert_id //ID del nuevo estudiante
         ]);
-    }else{
-           echo json_encode([
-            //Si no es post, retornar error
-            'exito'=> 'false',
-            'mensaje' => 'método no permitido'
+    } else {
+        echo json_encode([
+            'exito' => 'false',
+            'mensaje' => 'Error al crear estudiante:' . $conexion->error
         ]);
     }
+} else {
+    echo json_encode([
+        //Si no es post, retornar error
+        'exito' => 'false',
+        'mensaje' => 'método no permitido'
+    ]);
 }
-?>
