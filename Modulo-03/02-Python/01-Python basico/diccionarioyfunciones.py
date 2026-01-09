30cc3de04dbad6dab5df765727bacd785c81e15a
Nasua <acollazocacho@gmail.com>
2026-01-09 09:57:18 +0100

    fix diccionario y funciones

--- Modulo-03/02-Python/01-Python basico/diccionarioyfunciones.py
+++ Modulo-03/02-Python/01-Python basico/diccionarioyfunciones.py
@@ -13,7 +13,7 @@ Diccionario en python se identifica {}
 
 contactos ={}
 def mostrar_menu():
-    print("n\ === GESTIÓN DE USUARIOS ===")
+    print("\n === GESTIÓN DE USUARIOS ===")
     print("1 - Añadir contacto")
     print("2 - Buscar contacto")
     print("3 - Listar todos los contactos")
@@ -21,14 +21,14 @@ def mostrar_menu():
 
 def anhadir_contactos():
     nombre = input("Escribe el nombre del contacto: ")
-    telefono = input ("Escribe el telefono del contacto: ")
+    telefono = input("Escribe el telefono del contacto: ")
     contactos[nombre] = telefono
-    print("Contacto % añadido correctamente" %nombre)
+    print("Contacto %s añadido correctamente" % nombre)
 
 def buscar_contacto():
     nombre = input("Nombre contacto a buscar: ")
     if nombre in contactos:
-        print("Telefono de %" %(nombre, contactos[nombre]))
+        print("Telefono de %s: %s" % (nombre, contactos[nombre]))
     else:
         print("No existe el contacto",nombre)
 
@@ -36,21 +36,21 @@ def listar_contactos():
     if len(contactos) == 0:
         print("No existen contactos")
     else:
-        print("n\--- LISTA DE CONTACTOS ---")
+        print("\n--- LISTA DE CONTACTOS ---")
         for nombre, telefono in contactos.items():
-            print("%" (nombre,telefono))
+            print("%s: %s" % (nombre, telefono))
 
 while True:
     mostrar_menu()
     opcion = input("\n Elige una opcion: ")
-    if opcion == 1:
+    if opcion == "1":
         anhadir_contactos()
-    elif opcion == 2:
+    elif opcion == "2":
         buscar_contacto()
-    elif opcion == 3:
+    elif opcion == "3":
         listar_contactos()
-    elif opcion == 4:
-        print("Hatsa pronto")
+    elif opcion == "4":
+        print("Hasta pronto")
         break
     else:
         print("Opcion no válida")
---
+++ Modulo-03/02-Python/02-Biblioteca/biblioteca.py
@@ -0,0 +1,6 @@
+# Sistema de biblioteca
+# clase Base(ppal) Material con los atributos (titulo, año, disponibilidad)
+# clase derivadas Libro y Revista con atributos especificos
+# Implementer un método para prestar y devolver material
+# Usar polimorfismo para mostrar la información del material
+# Gestionar una coleccion de materiales
